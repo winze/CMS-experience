e35b6f276cc6c795ae8d72694b9ccf204f03af07
vinzcoco <vinzcoco@gmail.com>
2012-08-29 14:45:19 +0200

    passage de sf2.1 à sf2.0.17

--- src/Acme/DemoBundle/Twig/Extension/DemoExtension.php
+++ src/Acme/DemoBundle/Twig/Extension/DemoExtension.php
@@ -4,7 +4,6 @@ namespace Acme\DemoBundle\Twig\Extension;
 
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
-use CG\Core\ClassUtils;
 
 class DemoExtension extends \Twig_Extension
 {
@@ -50,12 +49,7 @@ EOF;
 
     protected function getControllerCode()
     {
-        $class = get_class($this->controller[0]);
-        if (class_exists('CG\Core\ClassUtils')) {
-            $class = ClassUtils::getUserClass($class);
-        }
-
-        $r = new \ReflectionClass($class);
+        $r = new \ReflectionClass($this->controller[0]);
         $m = $r->getMethod($this->controller[1]);
 
         $code = file($r->getFilename());
